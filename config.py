7c02d5299df1c1fdfde00fb654048f9f8a762a4b
4g <apurvagup@gmail.com>
2013-05-20 23:46:28 +0530

    Create config.py
    Default config. Requires sklearn for the classifiers

---
+++ config.py
@@ -0,0 +1,5 @@
+classifiers = {
+               "sklearn.tree":{"DecisionTreeClassifier"},
+               "sklearn.svm":{"SVC","NuSVC"},
+               "sklearn.ensemble":{"RandomForestClassifier","ExtraTreesClassifier"}
+               }
